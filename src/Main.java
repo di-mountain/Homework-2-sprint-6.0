47448ceaf6e4ef2720cc61f126b766ae4fb54f09
dimountain <gftght@rambler.ru>
2022-12-19 14:30:38 +0300

    Выполнил задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int sallary = 15000;
+        int total = 0;
+        int month = 1;
+        while (total < 2_459_000) {
+            total += sallary;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+            month++;
+        }
     }
 }
