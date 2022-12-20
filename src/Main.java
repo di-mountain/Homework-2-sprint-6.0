9729a2c1f2489f52ea59cc51147b73b75c3b697c
dimountain <gftght@rambler.ru>
2022-12-20 19:10:10 +0300

    Выполнил задачу 6

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
     }
 
     public static void task1() {
@@ -73,5 +74,20 @@ public class Main {
             month++;
         }
         System.out.println("Итого: за " + (month - 1) + " месяцев накоплено " + total + " рублей");
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        int sallary = 15000;
+        int total = 0;
+        int month = 1;
+        for (; month <= 108; month++) {
+            total += total / 100 * 7;
+            total += sallary;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+            }
+        }
+        System.out.println("Итого: за " + (month - 1) + " месяцев (9 лет) накоплено " + total + " рублей");
     }
 }
